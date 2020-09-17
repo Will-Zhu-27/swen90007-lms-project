8042093ee8ca6fb2aa161fbb8a3e70983bb9dc0d
marodriguez <marodriguez@unimelb.edu.au>
2020-09-17 20:06:13 +1000

    Initial commit

---
+++ setupDB.sql
@@ -0,0 +1,6 @@
+CREATE TABLE subjects(
+   code   VARCHAR(20),
+   name  VARCHAR(50),
+   coordinator VARCHAR(50),
+   PRIMARY KEY (code)
+);
---
+++ src/datasource/DBConnection.java
@@ -0,0 +1,32 @@
+package datasource;
+
+import java.sql.*;
+
+public class DBConnection {
+
+	public static PreparedStatement prepare(String stm) throws SQLException {
+		PreparedStatement preparedStatement = null;
+		try {
+			Connection dbConnection = getDBConnection();
+			preparedStatement = dbConnection.prepareStatement(stm);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return preparedStatement;
+	}
+
+	private static Connection getDBConnection() {
+		try {
+			DriverManager.registerDriver(new org.postgresql.Driver());
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
+			Connection dbConnection = DriverManager.getConnection(dbUrl);
+			return dbConnection;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Connection problem");
+		return null;
+
+	}
+
+}
